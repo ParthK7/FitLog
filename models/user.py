dba47eb75fbd7075afa1e0e35e1f4bd850de88a1
ParthK7 <parthkulkarni15@gmail.com>
2025-08-22 18:14:55 -0700

    Alembic initialized. User table schema defined and table created in database.

---
+++ models/user.py
@@ -0,0 +1,28 @@
+from .base import Base
+from datetime import datetime
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.sql import func 
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(Base):
+    __tablename__ = "users"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key = True)
+
+    username : Mapped[str] = mapped_column(String(50), unique = True, index = True)
+
+    hashed_password : Mapped[str] = mapped_column(String(100))
+    created_at : Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        nullable = False
+    )
+
+    updated_at : Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        onupdate = func.now(),
+        nullable = False
+    )
+
+    email : Mapped[str] = mapped_column(String(100), unique = True)
